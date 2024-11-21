5982938d2e767c10373ce760fea6b32123b8778b
turbocrime <turbocrime@users.noreply.github.com>
2024-11-21 00:18:54 -0800

    test treenode

--- src/TreeNodeCbor.sol
+++ src/TreeNodeCbor.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.28;
 
 import "./CidCbor.sol";
 
-library TreeNodeDataCbor {
+library TreeNodeCbor {
   using CBORDecoder for bytes;
 
   struct TreeNode {
--- test/CommitCbor.t.sol
+++ test/CommitCbor.t.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.17;
 import {Test, console} from "forge-std/Test.sol";
 import "../src/CborDecode.sol";
 import "../src/CommitCbor.sol";
-contract BskyRepoCBORTest {
+
+contract CommitCborTest {
     function test_readCommit() public pure {
         bytes memory rootCommitData = hex"a56364696478206469643a706c633a6d74713365346d67743777796a6868616e69657a656a3637637265766d336c61796b6c746f73703232716464617461d82a5825000171122066da6655bf8da79b69a87299cf170fed8497fa3059379dc4a8bfe1e28cab5d936470726576f66776657273696f6e03";
 
